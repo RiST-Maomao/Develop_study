31d1c91443f9a0bf31c5cd080a4538d3150c966d
nayu <is0762ix@ed.ritsumei.ac.jp>
2026-02-04 16:52:11 +0900

    AddVer1: Register/In Progress
    とりあえず出せるところまで
    余裕があればブラッシュアップ

--- Projects/Frontend/src/WorkSpace/Pages/Register/Register.tsx
+++ Projects/Frontend/src/WorkSpace/Pages/Register/Register.tsx
@@ -1,21 +1,52 @@
 import "./Register.css"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 import InputField from "../../Components/InputField/InputField";
-import ConfirmButton from "../../Components/ConfirmButton/ConfirmButton";
+import GrowButton from "../../Components/GrowButton/GrowButton";
 
 function Register() {
+    const navigate = useNavigate();
+    const [PlayerName, setPlayerName] = useState("");
+    const [GeminiAPI, setGeminiAPI] = useState("");
+    const [error, setError] = useState("");
+
     return (
-        <div className="LoginRegisterBackground">
-            <h1><span className="Pink">〇〇</span>
-                <span className="Blue">人狼</span>
-            </h1>
-            <div className="LoginRegisterBox">
-                <div className="LoginRegisterText">
-                    <InputField name="UserID" type="text" placeholder="ユーザー名" />
-                    <InputField name="Password" type="password" placeholder="パスワード" />
+        <form action="/RoomID" method="POST">
+            <div className="RegisterBackground">
+                <div className="RegisterBox">
+                    <div className="RegisterInput">
+                        <p>プレイヤー名</p>
+                        <InputField name="PlayerName" type="text"
+                            onChange={(e) => {
+                                setPlayerName(e.target.value);
+                                setError("");
+                            }}
+                            placeholder="" />
+                            {error && <p className="RegisterError">{error}</p>}
+                    </div>
+                    <div className="RegisterInput">
+                        <p>Gemini API</p>
+                        <InputField name="GeminiAPI" type="password"
+                            onChange={(e) => {
+                                setGeminiAPI(e.target.value);
+                                setError("");
+                            }}
+                            placeholder="" />
+                            {error && <p className="RegisterError">{error}</p>}
+                    </div>
                 </div>
-                <ConfirmButton label="スタート" onClick={() => window.location.href = "/LoginRegister"} type="button" />
+                <GrowButton label="決定" 
+                onClick={() => {
+                        if (PlayerName === "" || GeminiAPI === "") {
+                            setError("未入力の項目があります");
+                        } else {
+                            toast.success("登録しました"); 
+                            navigate("/RoomID");
+                        }
+                    }} className="RegisterButton" type="button" />
             </div>
-        </div>
+        </form>
     )
 }
 
