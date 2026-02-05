7eef11b6ff0fd5318003b201c0d0e5d17343f678
nayu <is0762ix@ed.ritsumei.ac.jp>
2026-02-05 14:46:28 +0900

    RoomID-In Progress
    見た目だけ

--- Projects/Frontend/src/App.tsx
+++ Projects/Frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Register from "./WorkSpace/Pages/Register/Register.tsx";
 import Home from "./WorkSpace/Pages/Home/Home.tsx";
+import RoomID from "./WorkSpace/Pages/RoomID/RoomID.tsx";
 
 function App() {
   return (
+    <>
+    <Toaster />
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} /> 
         <Route path='/Register' element={<Register />} /> 
+        <Route path='/RoomID' element={<RoomID />} /> 
       </Routes>
     </BrowserRouter>
+    </>
   )
 }
 
